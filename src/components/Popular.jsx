89a7cad285b5f761cb1dd39de2c7df42c9704572
yuukidiv <yuukigpatriarca@gmail.com>
2022-10-10 22:53:36 +0200

    created the carousel and made cards

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,6 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
+// styled is to eliminate classes
+import styled from "styled-components";
+// splide does the carousel and splide slide select each image as the card
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import '@splidejs/react-splide/css';
+
 function Popular() {
+// We create a variable that is responsive with the UI setPopular is like the function that modifies popular
+  const [popular, setPopular] = useState([]);
 // We use useEffect to mount the function popularGet as soon as possible when we refresh the page. This is the reason 
 useEffect(() => {
   popularGet();
@@ -13,14 +21,62 @@ Its just a condition we are putting to reload the useEffect */
     // MAKE SURE THE API IS CALLED REACT_APP_SOMETHING because if not it will not work
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_RECIPESS_API}&number=9`);
     const data = await api.json();
-    console.log(data);
+    setPopular(data.recipes);
   };
 
   
-
+// we are looping inside each recipe and give them the name recipe (just like a key)
   return (
-    <div>Popular</div>
+    <div>
+      <Wrap>
+        <h3>Popular picks</h3>
+        <Splide 
+          options={{
+            pagination: false,
+            perPage: 4,
+            drag: "free",
+            gap: '2rem'
+
+          }}>
+        {popular.map((recipe) => {
+          return (
+            <SplideSlide key={recipe.id}>
+              <Cards>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title} />
+              </Cards>
+            </SplideSlide>
+          )
+        })}
+        </Splide>
+      </Wrap>
+    </div>
   )
 }
 
+// Using the package styled components to create and style things without using classes or whatever
+const Wrap = styled.div`
+margin: 4rem 0 rem;
+`;
+
+const Cards = styled.div`
+  min-height: 15rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+  }
+`;
+
+
 export default Popular
+
+// back ticks ``
