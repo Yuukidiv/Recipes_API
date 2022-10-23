003ed9be8ef042a21c9f5c5467dc59d8da970983
yuukidiv <yuukigpatriarca@gmail.com>
2022-10-23 22:46:25 +0200

    connected the description page to popular and veggie

--- src/pages/Description.jsx
+++ src/pages/Description.jsx
@@ -8,32 +8,48 @@ function Description() {
     const [recipeDetails, setRecipeDetails] = useState({});
     const [activePage, setActivePage] = useState('instructions');
 
-    const fetchingData = async () => {
+    const fetchinData = async () => {
         const data = await fetch(`https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_RECIPESS_API}`);
         const detailData = await data.json();
-        setRecipeDetails(detailData)
+        setRecipeDetails(detailData);
+        console.log(detailData)
     };
     useEffect(() => {
-        fetchingData();
+        fetchinData();
     }, [params.name]);
 
-  return <BigDiv>
+  return (<BigDiv>
             <div>
                 <h2>{recipeDetails.title}</h2>
-                <img src={recipeDetails.image}/>
+                <img src={recipeDetails.image} alt='desiredphoto' />
             </div>
             <Info>
                 {/*when we click on the button we run a function that set the variable named activePage as what, then we check with an if statament and run the .active code in the styled component */}
                 <Button className={activePage === 'instructions' ? 'active' : ''} onClick={() => setActivePage('instructions')}>Instructions</Button>
                 <Button className={activePage === 'ingredients' ? 'active' : ''} onClick={() => setActivePage('ingredients')}>Ingredients</Button>
+                {activePage === 'instructions' && (
+                <div>
+                    <h3 dangerouslySetInnerHTML={{ __html: recipeDetails.summary}}></h3>
+                    <h2 className='step'>Step by step:</h2>
+                    <h3 dangerouslySetInnerHTML={{ __html: recipeDetails.instructions}}></h3>
+                </div>
+                )}
+                {activePage === 'ingredients' && (
+                <ul>
+                   {recipeDetails.extendedIngredients.map((ingredient) => ( 
+                        <li key={ingredient.id}>{ingredient.original}</li>
+                   ))}
+                </ul>
+                )}  
             </Info>
         </BigDiv>
+        )
   
 }
 const BigDiv = styled.div`
 
-margin-top: 10rem;
-margin-bottom: 10rem;
+margin-top: 8rem;
+margin-bottom: 5rem;
 display: flex;
 
 .active {
@@ -43,13 +59,16 @@ display: flex;
 h2 {
     margin-bottom: 2rem;
 };
+.step {
+    margin-top: 2rem;
+}
 li {
-    font-size: 1.2rem;
-    line-height: 2.5rem;
-};
+    margin: 1.3rem;
+}
 ul {
-    margin-top: 2.5rem;
-};
+    font-size: 1.3rem;
+}
+
 
 `;
 
@@ -61,6 +80,7 @@ border: 2px solid black;
 margin-right :2rem;
 font-weight: 600;
 border-radius: .7rem;
+margin-bottom: 1rem;
 
 :hover {
     background: black;
@@ -69,6 +89,7 @@ border-radius: .7rem;
 `;
 const Info = styled.div`
 margin-left: 5rem;
+
 `;
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Popular from '../components/Popular'
 import Veggie from '../components/Veggie'
-import Categories from '../components/Categories'
 function Home() {
   return (
     <div> 
