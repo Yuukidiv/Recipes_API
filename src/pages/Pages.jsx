4a4259395316aa757d6565a3a1d7aaba1f86371e
yuukidiv <yuukigpatriarca@gmail.com>
2022-10-24 23:16:14 +0200

    finished the project, needs restyle and more implementation

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -4,16 +4,20 @@ import Cuisine from './Cuisine'
 import SearchItems from './SearchItems'
 import Description from './Description'
 // making the routing (changing the route with the path we choose to render)
-import {Route, Routes } from 'react-router-dom'
+import {Route, Routes, useLocation} from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
 function Pages() {
+  const location = useLocation()
     // Browserouter does the browser, routes is the big div and with route we are choosing a path and to that we are giving it our element to render out.
   return (
-    <Routes>
+    <AnimatePresence exitBeforeEnter>
+    <Routes location={location} key={location.pathname }>
         <Route path="/" element={<Home />}/>
         <Route path="/cuisine/:type" element={<Cuisine />}/>
         <Route path="/search/:search" element ={<SearchItems />}/>
         <Route path="/description/:name" element ={<Description />}/>
     </Routes>
+    </AnimatePresence>
     )
 }
 
